ac77b334ddde5ee4ff56f9a5f8c447905c0f7607
Blake Netzeband <blake.netzeband@gmail.com>
2024-11-26 10:37:52 -0700

    clone in exercises

---
+++ basic-data-types.rb
@@ -0,0 +1,63 @@
+# NUMBERS
+# Addition
+1 + 1   #=> 2
+
+# Subtraction
+2 - 1   #=> 1
+
+# Multiplication
+2 * 2   #=> 4
+
+# Division
+10 / 5  #=> 2
+
+# Exponent
+2 ** 2  #=> 4
+3 ** 4  #=> 81
+
+# Modulus (find the remainder of division)
+8 % 2   #=> 0  (8 / 2 = 4; no remainder)
+10 % 4  #=> 2  (10 / 4 = 2 with a remainder of 2)
+
+# integer vs float
+# to convert int to float, use a float in arithmetic
+# example:
+17 / 5    #=> 3, not 3.4
+17 / 5.0  #=> 3.4
+
+# or conversion method
+# To convert an integer to a float:
+13.to_f   #=> 13.0
+
+# To convert a float to an integer:
+13.0.to_i #=> 13
+13.9.to_i #=> 13
+
+# STRINGS
+
+# concatenation
+# With the plus operator:
+"Welcome " + "to " + "Odin!"    #=> "Welcome to Odin!"
+
+# With the shovel operator:
+"Welcome " << "to " << "Odin!"  #=> "Welcome to Odin!"
+
+# With the concat method:
+"Welcome ".concat("to ").concat("Odin!")  #=> "Welcome to Odin!"
+
+# interpolation only works with "" (double quotes)
+
+name = "Odin"
+
+puts "Hello, #{name}" #=> "Hello, Odin"
+puts 'Hello, #{name}' #=> "Hello, #{name}"
+
+# SYMBOLS
+
+# unlike strings, symbols are stored in memory only once
+
+"string" == "string"  #=> true
+
+"string".object_id == "string".object_id  #=> false
+
+:symbol.object_id == :symbol.object_id    #=> true
